ab97a2cb71892e785818457e600a122cf11ead07
ADE OLAOYE <adeolaoye56@gmail.com>
2023-07-31 17:54:28 +0100

    complete logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,3 +16,95 @@ let endScreen = document.getElementById('end-screen');
 let currentQuestionIndex = 0;
 let time = 60;
 let counterId;
+
+// Start quiz and hide frontpage
+
+function quizStart() {
+  counterId = setInterval(countDown, 1000); // start countdown
+  counter.textContent = time;
+  startScr.setAttribute('class', 'hide'); // make initial heading hide
+  questionsDiv.removeAttribute('class'); // remove hide class
+  getQuestion(); // activate/call the function
+}
+
+// Loop through array of questions
+
+function getQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  questionPrompt.textContent = currentQuestion.prompt;
+  choices.innerHTML = '';
+  currentQuestion.options.forEach(function (choice, i) {
+    let choiceBtn = document.createElement('button');
+    choiceBtn.setAttribute('value', choice);
+    choiceBtn.textContent = i + 1 + '. ' + choice;
+    choiceBtn.onclick = answerClick;
+    choices.appendChild(choiceBtn); // adds button to choices div
+  });
+}
+
+// Check for right answers and penalize for wrong answer.Then next question
+
+function answerClick() {
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    time -= 10;
+    if (time < 0) {
+      time = 0;
+    }
+    counter.textContent = time;
+    feedback.textContent = `Wrong!`;
+  } else {
+    feedback.textContent = 'Correct!';
+  }
+  feedback.setAttribute('class', 'feedback');
+  setTimeout(function () {
+    feedback.setAttribute('class', 'feedback hide');
+  }, 2000);
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
+
+// End quiz 
+
+function quizEnd() {
+  clearInterval(counterId);
+  endScreen.removeAttribute('class');
+  let finalScore = document.getElementById('final-score');
+  finalScore.textContent = time;
+  questionsDiv.setAttribute('class', 'hide');
+}
+
+// End quiz when counter is 0
+
+function countDown() {
+  time--;
+  counter.textContent = time;
+  if (time === 0) {
+    quizEnd();
+  }
+}
+
+// Save score in local storage
+
+function saveHighscore() {
+  let name = initials.value.trim();
+  if (name !== '') {
+    let highscores =
+      JSON.parse(window.localStorage.getItem('highscores')) || [];
+    let newScore = {
+      score: time,
+      name: name,
+    };
+    highscores.push(newScore);
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
+  }
+}
+
+submitBtn.addEventListener('click', saveHighscore);
+
+// Start quiz after clicking start quiz
+
+startBtn.addEventListener('click', quizStart);
